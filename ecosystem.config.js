915662d4bd1045fd85a27e54d8436644f3b4e3c5
emresahin19 <81564669+emresahin19@users.noreply.github.com>
2024-09-12 20:14:56 +0300

    main - pm2 updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
         {
             name: 'demo-www',
             script: 'node_modules/next/dist/bin/next',
-            cwd: '/home/web/asimthecat.com/demo/server/apps/www',
+            cwd: '/home/web/thecacao.com.tr/thecacao-server/apps/www',
             args: 'start -p 4020',
             env: {
                 NODE_ENV: 'production',
@@ -18,7 +18,7 @@ module.exports = {
         {
             name: 'demo-dash',
             script: 'node_modules/next/dist/bin/next',
-            cwd: '/home/web/asimthecat.com/demo/server/apps/dash',
+            cwd: '/home/web/thecacao.com.tr/thecacao-server/apps/dash',
             args: 'start -p 4030',
             env: {
                 NODE_ENV: 'production',
@@ -27,7 +27,7 @@ module.exports = {
         {
             name: 'demo-api',
             script: 'apps/api/dist/src/main.js',
-            cwd: '/home/web/asimthecat.com/demo/server',
+            cwd: '/home/web/thecacao.com.tr/thecacao-server',
             env: {
                 NODE_ENV: 'production',
                 PORT: 4040,
