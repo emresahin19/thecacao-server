3559571baae9ae28a6e8e96236e213c230b51c79
emresahin19 <81564669+emresahin19@users.noreply.github.com>
2024-09-12 22:10:18 +0300

    main - critical css

--- lib/layouts/container.tsx
+++ lib/layouts/container.tsx
@@ -1,8 +1,6 @@
 import Header from '../components/Layout/components/www/header.component';
 import Footer from '../components/Layout/components/www/footer.component';
 import Modal from '../components/Modal/components/modal.component';
-// import dynamic from 'next/dynamic';
-// const Modal = dynamic(() => import('../components/Modal/components/modal.component'));
 
 const Container = ({children}: {children: React.ReactNode}) => {
     return (
