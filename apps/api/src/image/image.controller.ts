c6b8f3ff2cc3433bb04ff84c8006ec049d0199e0
emresahin19 <81564669+emresahin19@users.noreply.github.com>
2024-09-13 17:29:53 +0300

    main - image handler

--- apps/api/src/image/image.controller.ts
+++ apps/api/src/image/image.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, ParseIntPipe } from '@nestjs/common';
 import { ImageService } from './image.service';
 import { CreateImageDto } from './dto/create-image.dto';
 import { UpdateImageDto } from './dto/update-image.dto';
+import { Response } from 'express'; 
+import { join } from 'path';
 
 @Controller('images')
 export class ImageController {
     constructor(private readonly imageService: ImageService) {}
 
+    @Get('crop,w=:width,h=:height,f=:format,q=:quality/*')
+    async getCroppedImage(
+        @Param('width', new ParseIntPipe()) width: number,  // ParseIntPipe ile integer olarak al
+        @Param('height', new ParseIntPipe()) height: number,  // ParseIntPipe ile integer olarak al
+        @Param('format') format: 'png' | 'webp',  // String olarak formatı al
+        @Param('quality', new ParseIntPipe()) quality: number,  // ParseIntPipe ile integer olarak al
+        @Param() params: any,
+        @Res() res: Response,
+    ) {
+        // Varsayılan değerler
+        const _width = !isNaN(width) && width || 300;  // Varsayılan genişlik
+        const _height = !isNaN(height) && height || 300;  // Varsayılan yükseklik
+        const _quality = !isNaN(quality) && quality || 80;  // Varsayılan kalite
+        const _format = format || 'webp';  // Varsayılan format
+        const imagePath = params['0']; 
+    
+        // Resmi sıkıştır ve dosya yolunu al
+        const compressedImagePath = await this.imageService.compressImage(imagePath, _width, _height, _format, _quality);
+        // Resmi tarayıcıda göster
+        res.sendFile(compressedImagePath);
+    }
     @Post()
     create(@Body() createImageDto: CreateImageDto) {
         return this.imageService.create(createImageDto);
@@ -19,6 +42,7 @@ export class ImageController {
 
     @Get(':id')
     findOne(@Param('id') id: string) {
+        console.log('params', id);
         return this.imageService.findOne(+id);
     }
 
--- apps/api/src/image/image.service.ts
+++ apps/api/src/image/image.service.ts
@@ -4,13 +4,24 @@ import { Repository } from 'typeorm';
 import { Image } from './entities/image.entity';
 import { CreateImageDto } from './dto/create-image.dto';
 import { UpdateImageDto } from './dto/update-image.dto';
+import sharp from 'sharp';
+import { join } from 'path';
+import { existsSync, mkdirSync } from 'fs';
 
 @Injectable()
 export class ImageService {
+    private readonly inputDir = join(__dirname, '..', '..', '..', '..', 'media', 'storage', 'image');
+    private readonly outputDir = join(__dirname, '..', '..', '..', '..', 'media', 'storage', 'output-images');
+  
     constructor(
         @InjectRepository(Image)
         private readonly imageRepository: Repository<Image>,
-    ) {}
+        
+    ) {
+        if (!existsSync(this.outputDir)) {
+            mkdirSync(this.outputDir);
+        }
+    }
 
     create(createImageDto: CreateImageDto): Promise<Image> {
         const image = this.imageRepository.create(createImageDto);
@@ -32,5 +43,35 @@ export class ImageService {
 
     async remove(id: number): Promise<void> {
         await this.imageRepository.delete(id);
+    }
+
+    async compressImage(imagePath: string, width: number, height: number, format: 'png' | 'webp', quality: number): Promise<string> {
+        const inputFilePath = join(this.inputDir, imagePath);
+    
+        // Dinamik dosya adı: resim adı + parametreler (width, height, format, quality)
+        const imageName = imagePath.split('/').pop()?.split('.')[0]; // Resmin adı (dosya uzantısı olmadan)
+        const outputFileName = `${imageName}-${width}x${height}-${quality}.${format}`;
+        const outputPath = join(this.outputDir, outputFileName);
+    
+        // Dosya zaten var mı kontrol et
+        if (existsSync(outputPath)) {
+            return outputPath; // Dosya zaten varsa onu döndür
+        }
+    
+        let image = sharp(inputFilePath).resize(width, height, {
+            fit: sharp.fit.cover, // Resmi tam kaplayacak şekilde kesme
+            position: sharp.strategy.entropy, // Odak noktasını seçme
+        });
+    
+        // Resmi ilgili formata göre sıkıştır
+        if (format === 'png') {
+            image = image.png({ quality: quality });
+        } else if (format === 'webp') {
+            image = image.webp({ quality: quality });
+        }
+    
+        // Resmi belirtilen formatta kaydet
+        await image.toFile(outputPath);
+        return outputPath;
     }
 }
