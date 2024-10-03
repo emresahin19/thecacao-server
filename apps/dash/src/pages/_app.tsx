2d7252e197a6b09284dffb0341db9b8fd4df5795
Emre <emr.shnn@yahoo.com>
2024-10-03 16:05:49 +0200

    mini

--- apps/dash/src/pages/_app.tsx
+++ apps/dash/src/pages/_app.tsx
@@ -1,13 +1,49 @@
 import { DTheApp } from 'lib/pages';
 import GlobalLoading from "lib/components/Loading/components/global-loading.component";
 import { AppPropsWithLayout } from "lib/interfaces";
+import { useEffect, useState } from "react";
+import MdDownloading from 'lib/assets/icon/svg/MdDownloading.svg'
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page)
 
+  const [deferredPrompt, setDeferredPrompt] = useState<Event | null>(null);
+  const [isInstallable, setIsInstallable] = useState(false);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e: Event) => {
+      console.log('beforeinstallprompt event:', e);
+      e.preventDefault();
+      setDeferredPrompt(e);
+      setIsInstallable(true);
+    };
+
+    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+
+    return () => window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+  }, []);
+  
+  const handleInstallClick = () => {
+    if (deferredPrompt) {
+      (deferredPrompt as any).prompt(); 
+      (deferredPrompt as any).userChoice.then((choiceResult: { outcome: string }) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('Kullanıcı yüklemeyi kabul etti.');
+        } else {
+          console.log('Kullanıcı yüklemeyi reddetti.');
+        }
+        setDeferredPrompt(null);
+      });
+    }
+  };
   return (
     <>
       <DTheApp>
+          {isInstallable && (
+            <button onClick={handleInstallClick} className="install-button">
+              <MdDownloading />
+            </button>
+          )}
         {getLayout(
           <>
             <GlobalLoading />
--- lib/pages/dash/dashboard.page.tsx
+++ lib/pages/dash/dashboard.page.tsx
@@ -5,49 +5,14 @@ import dashRoutes from "lib/utils/dash-route.config";
 import { SidebarItemProps } from 'lib/interfaces';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import MdDownloading from 'lib/assets/icon/svg/MdDownloading.svg'
 
 const DashboardPage = () => {
   const routes = dashRoutes.flatMap((route) => route.children || route);
   const router = useRouter();
-  const [deferredPrompt, setDeferredPrompt] = useState<Event | null>(null);
-  const [isInstallable, setIsInstallable] = useState(false);
-
-  useEffect(() => {
-    const handleBeforeInstallPrompt = (e: Event) => {
-      console.log('beforeinstallprompt event:', e);
-      e.preventDefault();
-      setDeferredPrompt(e);
-      setIsInstallable(true);
-    };
-
-    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-
-    return () => window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-  }, []);
-  
-  const handleInstallClick = () => {
-    if (deferredPrompt) {
-      (deferredPrompt as any).prompt(); 
-      (deferredPrompt as any).userChoice.then((choiceResult: { outcome: string }) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('Kullanıcı yüklemeyi kabul etti.');
-        } else {
-          console.log('Kullanıcı yüklemeyi reddetti.');
-        }
-        setDeferredPrompt(null);
-      });
-    }
-  };
 
   return (
     <>
       <div className="dash-container">
-          {isInstallable && (
-            <button onClick={handleInstallClick} className="install-button">
-              <MdDownloading />
-            </button>
-          )}
         <div className="dash-apps">
           {routes.map(({ label, icon, href }: SidebarItemProps) => (
             href != router.pathname && (
